b0cacff44e7d06668d6ad73b109326b0f33a993b
UPHM <UPHM@UPHM-PC>
2016-08-20 16:10:55 +0700

    first commit

--- src/leadprogramme.java
+++ src/leadprogramme.java
@@ -16,7 +16,7 @@ public class leadprogramme {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(flag);
+   System.out.println("Flag");
     }
     
 }
